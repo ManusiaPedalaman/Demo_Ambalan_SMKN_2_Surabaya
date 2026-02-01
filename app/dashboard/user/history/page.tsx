13710ac4a38597471db5adec7b5e9b0226da050c
Si_Alan <TheGalipat313@gmail.com>
2026-02-01 18:21:06 +0700

    revisi

--- app/dashboard/user/history/page.tsx
+++ app/dashboard/user/history/page.tsx
@@ -151,7 +151,7 @@ export default function UserHistoryPage() {
     };
 
     return (
-        <div className="space-y-6">
+        <div className="p-8 font-dm-sans space-y-6">
             <h1 className="text-2xl font-bold text-gray-800">Riwayat & Aktivitas</h1>
             
              <Tabs activeTab={activeTab} onTabChange={setActiveTab}>
